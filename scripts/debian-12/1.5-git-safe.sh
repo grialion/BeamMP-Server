9827e13164f4f4701e644f21533183fb77393210
grialion <48643945+grialion@users.noreply.github.com>
2025-06-27 13:37:05 +0200

    ci: submodule init in git scripts

--- scripts/debian-12/1.5-git-safe.sh
+++ scripts/debian-12/1.5-git-safe.sh
@@ -5,3 +5,5 @@ set -ex
 git config --global --add safe.directory $(pwd)
 git config --global --add safe.directory $(pwd)/vcpkg
 git config --global --add safe.directory $(pwd)/deps/commandline
+git submodule init
+git pull --recursive
